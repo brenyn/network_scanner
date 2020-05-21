e0818870c8f038226f1b8481a475e088dd1650f1
Brenyn <brenynk@gmail.com>
2020-05-20 20:26:20 -0400

    resolved scapy.ARP issue with not being able to handle a range of IP addresses by reverting to scapy 2.4.2 in a virtual environment, seems to be an issue with scapy version 2.4.3

--- network_scanner.py
+++ network_scanner.py
@@ -5,18 +5,19 @@
 # Author: Brenyn Kissoondath
 # Course: Learn Python and Ethical Hacking From Scratch - StationX
 # Instructor: Zaid Al Quraishi
-# Purpose: 
+# Purpose: Create a network scanner that will return all IPs on a network w/ MAC addresses
 # Input(s): 
 # Output(s): 
 #
-# Notes to self: 
+# Notes to self: problems with passing range of IPs into scapy.ARP
 #
 ##########################################################################################################
 
-import os
 import scapy.all as scapy
 
 def scan(ip):
-	scapy.arping(ip)
+	arp_request = scapy.ARP(pdst=ip) #create an instance of scapy ARP class
+	print(arp_request.summary())
+	scapy.ls(scapy.ARP())# scapy.ls returns all variables contained in ARP class
 
 scan("10.0.2.1/24")
